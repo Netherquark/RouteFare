35ad1acd92af7b546313c2691de5dc7b011ce8ba
Netherquark <netherquark@protonmail.com>
2025-04-01 11:31:38 +0530

    improve skeleton for osrm

---
+++ src/com/routefare/osm/OpenSourceRoutingMachineClient.java
@@ -0,0 +1,9 @@
+package com.routefare.osm;
+
+public class OpenSourceRoutingMachineClient {
+    //implement route fetcher from OSRM
+
+    //convert json into r-tree/kd-tree/quadtree
+
+    //check toll distance for each toll using near route or haversine distance
+}
